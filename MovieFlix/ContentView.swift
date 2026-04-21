0baff750288c91811c12519897b56a8c2a35ec8c
김도윤 <kdy0096@gmail.com>
2026-04-22 00:37:59 +0900

    Initial Commit

---
+++ MovieFlix/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MovieFlix
+//
+//  Created by yoonie on 4/22/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MovieFlix/MovieFlixApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieFlixApp.swift
+//  MovieFlix
+//
+//  Created by yoonie on 4/22/26.
+//
+
+import SwiftUI
+
+@main
+struct MovieFlixApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
