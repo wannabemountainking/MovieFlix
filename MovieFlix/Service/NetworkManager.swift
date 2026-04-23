beb2c5bd4bb1d4b2a70996bde19d21d59a9e938b
wannabemountainking <kdy0096@gmail.com>
2026-04-23 17:02:32 +0900

    NetworkManager, ViewModel

---
+++ MovieFlix/Service/NetworkManager.swift
@@ -0,0 +1,65 @@
+//
+//  NetworkManager.swift
+//  MovieFlix
+//
+//  Created by YoonieMac on 4/23/26.
+//
+
+import Foundation
+import Combine
+
+/*
+ baseURL: https://api.themoviedb.org/3
+ apiKey: "26bc3a347d35e9e11f08e0ac83a85319"
+ language: "ko-KR"
+ 
+ 필요 데이터 URL파편
+ 1. nowPlaying : /movie/now_playing
+ 2. popular: /movie/popular
+ 3. topRated: /movie/top_rated
+ 4. upcoming: /movie/upcoming
+ */
+
+enum MovieCategory: CaseIterable {
+	case nowPlaying
+	case popular
+	case topRated
+	case upcoming
+	
+	var endpoint: String {
+		switch self {
+		case .nowPlaying: return "/movie/now_playing"
+		case .popular: return "/movie/popular"
+		case .topRated: return "/movie/top_rated"
+		case .upcoming: return "/movie/upcoming"
+		}
+	}
+}
+
+struct NetworkManager {
+	private let baseURL: String = "https://api.themoviedb.org/3"
+	private let apiKey: String = "26bc3a347d35e9e11f08e0ac83a85319"
+	private let language: String = "ko-KR"
+	
+	func getURL(category: MovieCategory, page: Int) -> URL? {
+		let movieCategory = category.endpoint
+		let urlString = "\(baseURL)\(movieCategory)?api_key=\(apiKey)&language=\(language)&page=\(page)"
+		return URL(string: urlString)
+	}
+	
+	func fetchMovies(category: MovieCategory, page: Int = 1) -> AnyPublisher<[Movie], Error> {
+		guard let url = getURL(category: category, page: page) else {
+			return Fail(error: URLError(.badURL))
+				.eraseToAnyPublisher()
+		}
+		
+		let networkPublisher = URLSession.shared.dataTaskPublisher(for: url)
+			.map { $0.data }
+			.decode(type: TMDBMovie.self, decoder: JSONDecoder())
+			.map { $0.movies }
+			.receive(on: DispatchQueue.main)
+			.eraseToAnyPublisher()
+		
+		return networkPublisher
+	}
+}
--- MovieFlix/ViewModel/MovieViewModel.swift
+++ MovieFlix/ViewModel/MovieViewModel.swift
@@ -11,5 +11,48 @@ import Combine
 
 final class MovieViewModel: ObservableObject {
     
+	let networkManager = NetworkManager()
+	
+	@Published var nowPlayingMovies: [Movie] = []
+	@Published var popularMovies: [Movie] = []
+	@Published var topRatedMovies: [Movie] = []
+	@Published var upcomingMovies: [Movie] = []
+	
+	var nextPages: [MovieCategory: Int] = [:]
+	var cancellables = Set<AnyCancellable>()
+	
+	init() {
+		for category in MovieCategory.allCases {
+			loadMovies(category: category)
+		}
+	}
+	
+	func loadMovies(category: MovieCategory, page: Int = 1) {
+		let networkPublisher = networkManager.fetchMovies(category: category, page: page)
+		networkPublisher
+			.sink { completion in
+				switch completion {
+				case .finished:
+					break
+				case .failure(let error):
+					print("network Error: \(error.localizedDescription)")
+				}
+			} receiveValue: { [weak self] movies in
+				guard let self else {return}
+				
+				switch category {
+				case .nowPlaying:
+					self.nowPlayingMovies.append(contentsOf: movies)
+				case .popular:
+					self.popularMovies.append(contentsOf: movies)
+				case .topRated:
+					self.topRatedMovies.append(contentsOf: movies)
+				case .upcoming:
+					self.upcomingMovies.append(contentsOf: movies)
+				}
+			}
+			.store(in: &cancellables)
+		
+	}
     
 }
