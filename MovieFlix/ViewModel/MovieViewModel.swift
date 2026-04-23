f9b08a8a4bd9d8ecc25a4034992991c53b5d07df
김도윤 <kdy0096@gmail.com>
2026-04-24 01:21:08 +0900

    MovieViewModel, CoreData MyMovie

--- MovieFlix/ViewModel/MovieViewModel.swift
+++ MovieFlix/ViewModel/MovieViewModel.swift
@@ -17,7 +17,7 @@ final class MovieViewModel: ObservableObject {
 	@Published var popularMovies: [Movie] = []
 	@Published var topRatedMovies: [Movie] = []
 	@Published var upcomingMovies: [Movie] = []
-	
+    
 	var nextPages: [MovieCategory: Int] = [:]
 	var cancellables = Set<AnyCancellable>()
 	
@@ -30,29 +30,53 @@ final class MovieViewModel: ObservableObject {
 	func loadMovies(category: MovieCategory, page: Int = 1) {
 		let networkPublisher = networkManager.fetchMovies(category: category, page: page)
 		networkPublisher
-			.sink { completion in
-				switch completion {
-				case .finished:
-					break
-				case .failure(let error):
-					print("network Error: \(error.localizedDescription)")
-				}
-			} receiveValue: { [weak self] movies in
-				guard let self else {return}
-				
-				switch category {
-				case .nowPlaying:
-					self.nowPlayingMovies.append(contentsOf: movies)
-				case .popular:
-					self.popularMovies.append(contentsOf: movies)
-				case .topRated:
-					self.topRatedMovies.append(contentsOf: movies)
-				case .upcoming:
-					self.upcomingMovies.append(contentsOf: movies)
-				}
-			}
-			.store(in: &cancellables)
-		
+            .sink(receiveCompletion: handleCompletion) { [weak self] movies in
+                guard let self else { return }
+                self.allocateMovies(category: category, page: page, movies: movies)
+            }
+            .store(in: &cancellables)
 	}
     
+    func loadNextPage(category: MovieCategory) {
+        nextPages[category, default: 1] += 1
+        loadMovies(category: category, page: nextPages[category, default: 1])
+    }
+    
+    private func handleCompletion(_ completion: Subscribers.Completion<Error>) {
+        switch completion {
+        case .finished:
+            break
+        case .failure(let error):
+            print("Error: \(error.localizedDescription)")
+        }
+    }
+    
+    private func allocateMovies(category: MovieCategory, page: Int, movies: [Movie]) {
+        switch category {
+        case .nowPlaying:
+            if page == 1 {
+                self.nowPlayingMovies = movies
+            } else {
+                self.nowPlayingMovies.append(contentsOf: movies)
+            }
+        case .popular:
+            if page == 1 {
+                self.popularMovies = movies
+            } else {
+                self.popularMovies.append(contentsOf: movies)
+            }
+        case .topRated:
+            if page == 1 {
+                self.topRatedMovies = movies
+            } else {
+                self.topRatedMovies.append(contentsOf: movies)
+            }
+        case .upcoming:
+            if page == 1 {
+                self.upcomingMovies = movies
+            } else {
+                self.upcomingMovies.append(contentsOf: movies)
+            }
+        }
+    }
 }
